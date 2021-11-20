a7e7bb96b4d05deb7d835fee44c125df2522b743
Asadullah Rifat <asadullah.rifat@selise.ch>
2021-11-20 19:56:35 +0600

    database service developed

---
+++ Worldescape.Database/DatabaseService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Worldescape.Database
+{
+    public class DatabaseService
+    {
+        #region Fields
+
+        private readonly string _connectionString;
+        private readonly ILogger<DatabaseService> _logger;
+
+        #endregion
+
+        #region Ctor
+        public DatabaseService(
+           IConfiguration configuration,
+           ILogger<DatabaseService> logger)
+        {
+            _connectionString = configuration.GetConnectionString("WorldescapeDatabase");
+            _logger = logger;
+        }
+        #endregion
+
+        #region Common
+
+        private IMongoCollection<T> GetCollection<T>()
+        {
+            var client = new MongoClient(_connectionString);
+            var database = client.GetDatabase("Worldescape");
+
+            var collectionName = typeof(T).Name + "s";
+
+            var collection = database.GetCollection<T>(collectionName);
+            return collection;
+        }
+
+        #endregion
+
+        public async Task<List<T>> GetDocuments<T>(FilterDefinition<T> filterDefinition)
+        {
+            var collection = GetCollection<T>();
+            var result = await collection.Find(filterDefinition).ToListAsync();
+
+            return result;
+        }
+
+        public async Task<bool> InsertDocument<T>(T document)
+        {
+            var collection = GetCollection<T>();
+
+            await collection.InsertOneAsync(document);
+
+            return true;
+        }
+
+        public async Task<bool> InsertDocuments<T>(IEnumerable<T> documents)
+        {
+            var collection = GetCollection<T>();
+
+            await collection.InsertManyAsync(documents);
+
+            return true;
+        }
+
+        public async Task<bool> ReplaceDocument<T>(T document, FilterDefinition<T> filterDefinition)
+        {
+            var collection = GetCollection<T>();
+
+            var result = await collection.FindOneAndReplaceAsync(filterDefinition, document);
+
+            return result != null;
+        }
+
+        public async Task<bool> DeleteDocument<T>(FilterDefinition<T> filterDefinition)
+        {
+            var collection = GetCollection<T>();
+
+            var result = await collection.FindOneAndDeleteAsync(filterDefinition);
+
+            return result != null;
+        }
+
+        public async Task<bool> DeleteDocuments<T>(FilterDefinition<T> filterDefinition)
+        {
+            var collection = GetCollection<T>();
+
+            var result = await collection.DeleteManyAsync(filterDefinition);
+
+            return result != null;
+        }
+    }
+}
---
+++ Worldescape.Database/DatabaseServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldescape.Database
+{
+    public static class DatabaseServiceExtensions
+    {
+        public static IServiceCollection AddDatabaseService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<DatabaseService>();
+            return serviceCollection;
+        }
+    }
+}
