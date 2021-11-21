bae812947e2042d34dbd26b4cad4c14addddee07
Asadullah Rifat <asadullah.rifat@selise.ch>
2021-11-21 23:47:31 +0600

    code cleanup

--- WorldescapeWebService.Core/Declarations/Commands/AddUserCommand.cs
+++ WorldescapeWebService.Core/Declarations/Commands/AddUserCommand.cs
@@ -8,39 +8,6 @@ namespace WorldescapeWebService.Core;
 /// </summary>
 public class AddUserCommand : AddUserCommandRequest, IRequest<ServiceResponse>
 {
-    ///// <summary>
-    ///// Name of the user.
-    ///// </summary>
-    //public string Name { get; set; } = string.Empty;
 
-    ///// <summary>
-    ///// The email address of the user.
-    ///// </summary>
-    //public string Email { get; set; } = string.Empty;
-
-    ///// <summary>
-    ///// The phone number of the user.
-    ///// </summary>
-    //public string Phone { get; set; } = string.Empty;
-
-    ///// <summary>
-    ///// The password of the user.
-    ///// </summary>
-    //public string Password { get; set; } = string.Empty;
-
-    ///// <summary>
-    ///// Image url of the user.
-    ///// </summary>
-    //public string ImageUrl { get; set; } = string.Empty;
-
-    ///// <summary>
-    ///// The gender of the user.
-    ///// </summary>
-    //public Gender Gender { get; set; }
-
-    ///// <summary>
-    ///// The user's birthday.
-    ///// </summary>
-    //public DateTime DateOfBirth { get; set; }
 }
 
--- WorldescapeWebService.Core/Declarations/Commands/AddWorldCommand.cs
+++ WorldescapeWebService.Core/Declarations/Commands/AddWorldCommand.cs
@@ -8,14 +8,6 @@ namespace WorldescapeWebService.Core;
 /// </summary>
 public class AddWorldCommand : AddWorldCommandRequest, IRequest<World>
 {
-    ///// <summary>
-    ///// Name of the world.
-    ///// </summary>
-    //public string Name { get; set; } = string.Empty;
 
-    ///// <summary>
-    ///// Image url of the world.
-    ///// </summary>
-    //public string ImageUrl { get; set; } = string.Empty;
 }
 
