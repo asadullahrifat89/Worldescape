11b1f3d9e99ee71fa548af5ea3ae0632b89513ee
Asadullah Rifat <asadullah.rifat@selise.ch>
2021-11-29 16:38:38 +0600

    ContentDialogueWindow developed

---
+++ Worldescape/Worldescape/Windows/ContentDialogueWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Worldescape
+{
+    public partial class ContentDialogueWindow : ChildWindow
+    {
+        readonly Action<bool> _result;
+
+        public ContentDialogueWindow(string title, string message, Action<bool> result)
+        {
+            InitializeComponent();
+            _result = result;
+            Title = title;
+            ContentScrollViewer.Content = message;
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            _result?.Invoke(true);
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _result!.Invoke(false);
+            this.DialogResult = false;
+        }
+    }
+}
+
