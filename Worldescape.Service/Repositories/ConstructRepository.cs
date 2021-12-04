0bc536cd7f4f55d846e3d6cfabede6554a9aaf07
Asadullah Rifat <asadullah.rifat@selise.ch>
2021-12-04 06:55:44 +0600

    repository pattern adding

--- Worldescape.Service/Repositories/ConstructRepository.cs
+++ Worldescape.Service/Repositories/ConstructRepository.cs
@@ -6,23 +6,6 @@ using Worldescape.Common;
 
 namespace Worldescape.Service
 {
-    public class RepositoryResponse
-    {
-        public object Result { get; set; }
-        public bool Success { get; set; }
-        public string Error { get; set; } = string.Empty;
-
-        public static RepositoryResponse BuildResponse(bool success, object result, string error = null)
-        {
-            return new RepositoryResponse
-            {
-                Error = error,
-                Success = success,
-                Result = result
-            };
-        }
-    }
-
     public class ConstructRepository
     {
         readonly HttpServiceHelper _httpServiceHelper;
---
+++ Worldescape.Service/Repositories/RepositoryResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Worldescape.Service
+{
+    public class RepositoryResponse
+    {
+        public object Result { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; } = string.Empty;
+
+        public static RepositoryResponse BuildResponse(bool success, object result, string error = null)
+        {
+            return new RepositoryResponse
+            {
+                Error = error,
+                Success = success,
+                Result = result
+            };
+        }
+    }
+}
